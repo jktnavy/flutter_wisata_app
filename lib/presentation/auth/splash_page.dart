df0f6af37ee5b0f91a33ea528e2c682a4aa839d9
Heden Satriya <heden.jma@gmail.com>
2024-07-02 09:35:50 +0700

    Import Flutter Template

---
+++ lib/presentation/auth/splash_page.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_wisata_app/core/core.dart';
+
+import '../../core/assets/assets.gen.dart';
+import 'login_page.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Future.delayed(
+      const Duration(seconds: 2),
+      () => context.pushReplacement(const LoginPage()),
+    );
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(96.0),
+        child: Center(
+          child: Assets.images.logoBlue.image(),
+        ),
+      ),
+      bottomNavigationBar: SizedBox(
+        height: 100.0,
+        child: Align(
+          alignment: Alignment.center,
+          child: Assets.images.logoCwb.image(width: 96.0),
+        ),
+      ),
+    );
+  }
+}
